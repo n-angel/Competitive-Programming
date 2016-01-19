d46c1aba940e0920afc63126a28b79a26b214a90
Nayeli Angel <nayeli.angel.p@gmail.com>
2016-01-19 00:20:37 -0600

    Problema: Helpful Maths

---
+++ codeforces/339A/339A.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+	int bucket[3] = {0, 0, 0};
+	char sum[105];
+	int ap = 0;
+
+	cin >> sum;
+	for(int i=0 ; i<strlen(sum) ; i+=2)
+	{
+		bucket[sum[i] - '1'] ++;
+	}
+
+	for(int i=0 ; i<3 ; i++)
+	{
+		for(int j=0 ; j<bucket[i] ; j++ )
+		{
+			sum[ap] = i + '1';
+			ap += 2;
+		}
+	}
+
+	cout << sum;
+}
---
+++ codeforces/339A/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _339A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] bucket = new int[3] { 0, 0, 0 };
+            string sum;
+            int i, j;
+
+            sum = Console.ReadLine();
+
+            char [] sumElements = sum.ToCharArray();
+            int ap = 0;
+
+            for (i = 0; i<sum.Length; i += 2)
+            {
+                bucket[sum[i] - '1']++;
+            }
+
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < bucket[i]; j++)
+                {
+                    sumElements[ap] = (char)(i + '1');
+                    ap += 2;
+                }
+            }
+
+            Console.WriteLine(sumElements);
+        }
+    }
+}
